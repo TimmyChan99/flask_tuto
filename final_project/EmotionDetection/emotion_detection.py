d288e03fb77c205428f526d7f520565ed5562f02
Fatima Ezzahra elmenoun <felemenoun@gmail.com>
2025-08-01 15:18:59 +0100

    Update emotion_detection.py

--- final_project/EmotionDetection/emotion_detection.py
+++ final_project/EmotionDetection/emotion_detection.py
@@ -1,23 +1,30 @@
 import requests
 import json
 
-def emotion_detector(text_to_analyse):
+def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    myobj = { "raw_document": { "text": text_to_analyse } }
+    myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers=headers)
 
-    response_text = response.text
-    response_json = response.json()
+    if response.status_code == 200:
+        response_text = response.text
+        response_json = response.json()
 
-    emotion_predictons = response_json.get('emotionPredictions', [])
-    emotions, *rest = emotion_predictons
+        emotion_predictions = response_json.get('emotionPredictions', [])
+        emotions, *rest = emotion_predictions
 
-    if emotion_predictons and emotions:
-        emotions_scores = emotions.get('emotion')
-        dominant_emotion = max(emotions_scores, key=emotions_scores.get)
-        dominant_emotion_score = emotions_scores.get(dominant_emotion)
-
-
-
-    return emotions_scores, { 'message': f'Dominant emotion is {dominant_emotion} with score of {dominant_emotion_score}'}
+        if emotion_predictions and emotions:
+            emotions_scores = emotions.get('emotion')
+            dominant_emotion = max(emotions_scores, key=emotions_scores.get)
+            
+            return { **emotions_scores, 'dominant_emotion': dominant_emotion }
+    
+    return {
+        'anger': None,
+        'disgust': None,
+        'fear': None,
+        'joy': None,
+        'sadness': None,
+        'dominant_emotion': None
+    }
